234992fc5af0819c78458003e530999ec108d3d9
Ivanka Tkachenko <ivanka.marie@gmail.com>
2020-10-05 10:03:36 -0400

    Changes to be committed: 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/.gitignore 	new file:   src/assets/fonts/LexendTera-Regular.ttf 	new file:   src/assets/fonts/OpenSans-Bold.ttf 	new file:   src/assets/fonts/OpenSans-BoldItalic.ttf 	new file:   src/assets/fonts/OpenSans-ExtraBold.ttf 	new file:   src/assets/fonts/OpenSans-ExtraBoldItalic.ttf 	new file:   src/assets/fonts/OpenSans-Italic.ttf 	new file:   src/assets/fonts/OpenSans-Light.ttf 	new file:   src/assets/fonts/OpenSans-LightItalic.ttf 	new file:   src/assets/fonts/OpenSans-Regular.ttf 	new file:   src/assets/fonts/OpenSans-SemiBold.ttf 	new file:   src/assets/fonts/OpenSans-SemiBoldItalic.ttf 	new file:   src/assets/fonts/Palanquin-Bold.ttf 	new file:   src/assets/fonts/Palanquin-ExtraLight.ttf 	new file:   src/assets/fonts/Palanquin-Light.ttf 	new file:   src/assets/fonts/Palanquin-Medium.ttf 	new file:   src/assets/fonts/Palanquin-Regular.ttf 	new file:   src/assets/fonts/Palanquin-SemiBold.ttf 	new file:   src/assets/fonts/Palanquin-Thin.ttf 	new file:   src/assets/images/me_and_rog.jpg 	new file:   src/assets/stylesheet.css 	new file:   src/index.html 	new file:   src/main.rs 	new file:   src/weekly.html 	new file:   target/.rustc_info.json 	new file:   target/CACHEDIR.TAG 	new file:   target/debug/.cargo-lock 	new file:   target/debug/.fingerprint/Stagecrew-0d0e59a06173a099/bin-Stagecrew 	new file:   target/debug/.fingerprint/Stagecrew-0d0e59a06173a099/bin-Stagecrew.json 	new file:   target/debug/.fingerprint/Stagecrew-0d0e59a06173a099/dep-bin-Stagecrew 	new file:   target/debug/.fingerprint/Stagecrew-0d0e59a06173a099/invoked.timestamp 	new file:   target/debug/.fingerprint/Stagecrew-0d0e59a06173a099/output-bin-Stagecrew 	new file:   target/debug/Stagecrew 	new file:   target/debug/Stagecrew.d 	new file:   target/debug/deps/Stagecrew-0d0e59a06173a099 	new file:   target/debug/deps/Stagecrew-0d0e59a06173a099.d 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/1fq2tzatpdofjtnt.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/2hatjh2u3jip7obd.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/3cxoblq2fb39wrp8.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/3zo63sc27tnb2i5r.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/4w9a0x1ybx6p1d7y.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/7bmyj7kcjcy3je1.o 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/dep-graph.bin 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/query-cache.bin 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu-1mexiuqu8rz6c/work-products.bin 	new file:   target/debug/incremental/Stagecrew-njky53ucw3zq/s-fra2z31h38-1hhzqtu.lock

---
+++ target/debug/Stagecrew.d
@@ -0,0 +1,1 @@
+/home/ivanka/Documents/Code/Git/Stagecrew/target/debug/Stagecrew: /home/ivanka/Documents/Code/Git/Stagecrew/src/main.rs
---
+++ target/debug/deps/Stagecrew-0d0e59a06173a099.d
@@ -0,0 +1,5 @@
+/home/ivanka/Documents/Code/Git/Stagecrew/target/debug/deps/Stagecrew-0d0e59a06173a099: src/main.rs
+
+/home/ivanka/Documents/Code/Git/Stagecrew/target/debug/deps/Stagecrew-0d0e59a06173a099.d: src/main.rs
+
+src/main.rs:
